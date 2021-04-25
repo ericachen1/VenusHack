d45fab12f852f32f86ebf6f408cabc5d1983ee5d
Avinash Upadhyaya <avinashupadhya99@gmail.com>
2021-04-25 23:08:53 +0530

    feat: display browser push notifications

--- js/script.js
+++ js/script.js
@@ -6,15 +6,23 @@ $(document).ready(function () {
         direction: 'down',
         intervalTime: 0
     });
+    if (!Notification) {
+        alert('Desktop notifications not available in your browser. Try Chromium.');
+        return;
+    }
+
+    if (Notification.permission !== 'granted')
+        Notification.requestPermission();
+
     $('#time').focusout(() => {
         startTimer();
     });
 
-    $('.refresh').click(function() {
+    $('.refresh').click(function () {
         startTimer();
     })
 
-    $('.radio-group .js-radio').click(function(){
+    $('.radio-group .js-radio').click(function () {
         $(this).parent().find('.js-radio').children('.radio-button-square').removeClass('radio-selected');
         $(this).children('.radio-button-square').addClass('radio-selected');
         var val = $(this).attr('data-value');
@@ -42,7 +50,7 @@ function startTimer() {
     // Start timer only if timeInMilliseconds is not empty
     if (timeInMilliseconds) {
         // Check if an activity was selected
-        if(selectedActivity) {
+        if (selectedActivity) {
             // Reset the filpTimer by resetting the html
             $('.timer-div').html('');
             $('.timer-div').append(`
@@ -55,12 +63,53 @@ function startTimer() {
             $('.flipTimer').flipTimer({
                 direction: 'down',
                 intervalTime: timeInMilliseconds,
-                callback:function() { displayModal(); }
+                callback: function () {
+                    displayModal();
+                    sendNotification();
+                }
             });
         } else {
             alert('Please selected an activity');
         }
     } else {
         alert('Please enter both the hours and minutes');
+    }
+}
+
+function sendNotification() {
+    if (Notification.permission !== 'granted')
+        Notification.requestPermission();
+    else {
+        var bodyText = 'Hey there! You\'ve been notified!';
+        switch(selectedActivity) {
+            case 'Physical Wellness':
+                bodyText = 'Your reminder to stand up and stretch';
+            break;
+            case 'Intellectual Wellness':
+                bodyText = '';
+            break;
+            case 'Spiritual Wellness':
+                bodyText = '';
+            break;
+            case 'Emotional Wellness':
+                bodyText = '';
+            break;
+            case 'Occupational Wellness':
+                bodyText = '';
+            break;
+            case 'Social Wellness':
+                bodyText = 'Your reminder to contact loved ones!';
+            break;
+            case 'Environmental':
+                bodyText = 'Your reminder to talk a walk outside';
+            break;
+        }
+        var notification = new Notification(selectedActivity, {
+            // icon: 'http://cdn.sstatic.net/stackexchange/img/logos/so/so-icon.png',
+            body: bodyText,
+        });
+        notification.onclick = function () {
+            window.open(window.location.href);
+        };
     }
 }
